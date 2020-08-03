436bcb856851b481f62072a72c3eed0a3592d0f7
Tzuwei Chen <chen.tzuwei@gmail.com>
2020-08-03 16:19:20 +0800

    add setting for modify toolbar

--- imkit/src/main/java/com/imkit/CustomChatFragment.java
+++ imkit/src/main/java/com/imkit/CustomChatFragment.java
@@ -2,12 +2,15 @@ package com.imkit;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.imkit.widget.fragment.IChatFragment;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CustomChatFragment extends com.imkit.widget.fragment.ChatFragment implements IChatFragment {
 
@@ -49,8 +52,11 @@ public class CustomChatFragment extends com.imkit.widget.fragment.ChatFragment i
             if (activity instanceof AppCompatActivity) {
                 AppCompatActivity appCompatActivity = (AppCompatActivity) activity;
                 if (appCompatActivity.getSupportActionBar() != null) {
-                    appCompatActivity.getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_nav_back);
-                    appCompatActivity.getSupportActionBar().setBackgroundDrawable(new ColorDrawable(0xFFFFFF));
+                    Drawable resImg = requireContext().getResources().getDrawable(R.drawable.ic_nav_back);
+                    resImg.setColorFilter(Color.parseColor(IMKIT.TOOLBAR_HOME_COLOR), PorterDuff.Mode.SRC_ATOP);
+                    appCompatActivity.getSupportActionBar().setHomeAsUpIndicator(resImg);
+
+                    appCompatActivity.getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor(IMKIT.TOOLBAR_BACKGROUND_COLOR)));
                 }
             }
         }
